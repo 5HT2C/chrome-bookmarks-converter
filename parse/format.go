23b33db4745fc64d736e8906edc5e57f3b9cb3f9
5HT2 <git@frogg.ie>
2024-10-25 00:52:01 -0400

    fix: (format) Missing attributes + failed browser check

--- parse/format.go
+++ parse/format.go
@@ -47,11 +47,11 @@ func (g GenChild) IsFolder() bool {
 }
 
 func (g GenChild) IsChrome() bool { // unreliable
-	return g.IsFolder() && g.Name == "Bookmarks Bar"
+	return g.IsFolder() && (g.Name == "Bookmarks Bar" || g.Name == "Bookmarks bar")
 }
 
 func (g GenChild) IsEdge() bool { // unreliable
-	return g.IsFolder() && g.Name == "Favorites Bar"
+	return g.IsFolder() && (g.Name == "Favorites Bar" || g.Name == "Favorites bar")
 }
 
 // AttrDefaultBar will return if the current folder is the default bookmark folder for either edge or chrome
@@ -63,14 +63,24 @@ func (g GenChild) AttrStr(m map[string]string) map[string]string {
 	m = utils.MapAppend(
 		m, []utils.MapCondition{
 			{
-				g.DateAdded != "" && g.DateAdded != "0",
+				!utils.StringEmpty(g.DateAdded),
 				"ADD_DATE",
 				g.DateAdded,
 			},
 			{
-				g.DateModified != "" && g.DateModified != "0",
+				!utils.StringEmpty(g.DateModified),
 				"LAST_MODIFIED",
 				g.DateModified,
+			},
+			{
+				utils.StringEmpty(g.DateModified) && !utils.StringEmpty(g.DateAdded),
+				"LAST_MODIFIED",
+				g.DateAdded,
+			},
+			{
+				!utils.StringEmpty(g.DateLastUsed),
+				"LAST_VISIT",
+				g.DateLastUsed,
 			},
 			{len(g.Guid) > 0, "GUID", g.Guid},
 			{g.AttrDefaultBar(), "PERSONAL_TOOLBAR_FOLDER", "true"},
